a67448ba771730bb7d9e1b527143a9aba7edf25c
Cale Smith <calepayson@mac.com>
2024-10-02 15:07:58 -0700

    feat: add solution to exercise 06
    - Add function that calculates whether a given year is a leap year.
    - Perform criteria calculation upfront for clarity.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divisibleByFour = (year % 4) === 0;
+    const divisibleBy100 = (year % 100) === 0;
+    const divisibleBy400 = (year % 400) === 0;
 
+    if (divisibleBy400) {
+        return true;
+    } else if (divisibleBy100) {
+        return false;
+    } else if (divisibleByFour) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
