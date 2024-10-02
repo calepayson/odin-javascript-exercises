94b48ef6921326844fe296d19ba6d5475812a0ad
Cale Smith <calepayson@mac.com>
2024-10-02 14:54:20 -0700

    feat: add solution to exercise 04
    - Add a function that iterates through all args and removes them from
      the provided array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array1, ...args) {
+    args.forEach((arg) => {
+        let index = array1.indexOf(arg);
+        while (index !== -1) {
+            array1.splice(index, 1);
+            index = array1.indexOf(arg);
+        }
+    });
+    return array1;
 };
 
 // Do not edit below this line
