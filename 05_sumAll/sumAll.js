100ceb11771aa934609e5be56b04748cf0bcf76f
Cale Smith <calepayson@mac.com>
2024-10-02 15:02:11 -0700

    feat: add solution to exercise 05
    - Sum all integers in the range provided.
    - Add error handling to ensure inputs are positive integers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let nonIntegerInput = !Number.isInteger(num1) || !Number.isInteger(num2);
+    if (nonIntegerInput){
+        return "ERROR";
+    }
 
+    let max = Math.max(num1, num2);
+    let min = Math.min(num1, num2);
+
+    if (min < 0) {
+        return "ERROR";
+    }
+
+    let result = 0;
+    for (let i = min; i <= max; i++) {
+        result = result + i;
+    }
+    return result;
 };
 
 // Do not edit below this line
